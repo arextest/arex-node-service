170435eb35a70218d1d087bcbefacd2c1d40148b
rchen9 <rchen9@trip.com>
2022-11-25 11:28:12 +0800

    style:code specification

--- src/apps/casesend/service/casehandlefactory.service.ts
+++ src/apps/casesend/service/casehandlefactory.service.ts
@@ -1,6 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { CaseRequest } from '../model/caserequest';
-import { CaseSendRequest } from '../model/casesendrequest';
 import { CaseHandleService } from './casehandle.service';
 import { DoubleCaseHandleService } from './doublecasehandle.service';
 import { NoramlCaseHandleService } from './noramlcasehandle.service';
